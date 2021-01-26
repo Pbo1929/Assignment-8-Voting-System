25bab8c2d7ac46070a946cd565c5b009fa586097
Prem Worapon Sumsai <lulztit1929@gmail.com>
2021-01-26 15:12:53 +0700

    Add project files.

---
+++ Assignment 8 Voting System/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Assignment_8_Voting_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vote[] Votes = new Vote[5000];
+            Votes[0] = new Vote("Bob", "Communists");
+            Votes[1] = new Vote("John", "Communists");
+            Votes[2] = new Vote("Sam", "Communists");
+            Votes[3] = new Vote("Ankit", "Democrats");
+            
+
+        }
+        static void CastVote(Vote[] Votes)
+        {
+            {
+                int End = 1;
+                while(End == 1)
+                {
+                    Console.WriteLine("VOTE FOR YOUR PARTY...");
+                    int x;
+                    Console.WriteLine("1 = COMMUNISTS, 2 = DEMOCRATS, 3 = FASCISTS");
+                    x = Console.Read();
+                    if 
+                    if ( x == 1 )
+                    {
+
+                    }
+                    if ( x == 2 )
+                    {
+
+                    }
+                    if ( x == 3)
+                    {
+
+                    }
+                }
+                
+            }
+        }
+    }
+}
---
+++ Assignment 8 Voting System/Vote.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_8_Voting_System
+{
+    class Vote
+    {
+        private string VoterName;
+        private string VoterParty;
+        private DateTime VoteTime;
+        public static int VoteCount;
+        
+
+        public Vote(string VoterName, string VoterParty)
+        {
+            this.VoterName = VoterName;
+            this.VoterParty = VoterParty;
+            VoteTime = DateTime.Now;
+            VoteCount++;
+        }
+
+        public int GetVotes()
+        {
+            return VoteCount;
+        }
+
+        public string GetVoterParty()
+        {
+            return VoterParty;
+        }
+    }
+}
